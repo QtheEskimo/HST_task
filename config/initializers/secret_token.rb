a1c77c75b54f6a1091a656ce8745e8a3bb21790a
Daniel Browne <daniel.browne@the-web-site.com>
2012-12-19 11:30:56 +0000

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HstTask::Application.config.secret_token = '40dcea5d6a1156dbc414ec09501dc20ae5ca8b877e5c2c6f63b613dfb0bd24b3165a9f8e0762e1aca86cdeaa4079ce20374720eb7def1b0195f15c251fad9ec7'

5f2de57884645489ca17bae6417e2a9e93813626
Daniel Browne <daniel.browne@the-web-site.com>
2012-12-19 12:51:10 +0000

    Adds random name method and and sets the records

--- app/controllers/collection_controller.rb
+++ app/controllers/collection_controller.rb
@@ -8,30 +8,32 @@ class CollectionController < ApplicationController
 
     collection1 = Collection.create(name: 'Collection 1', position: 1)
 
-      item1 = Item.create(name: 'random1', position: 1, collection: collection1)
-      item2 = Item.create(name: 'random2', position: 2, collection: collection1)
-      item3 = Item.create(name: 'random3', position: 3, collection: collection1)
-      item4 = Item.create(name: 'random4', position: 4, collection: collection1)
-      item5 = Item.create(name: 'random5', position: 5, collection: collection1)
+      item1 = Item.create(name: Item.random_name, position: 1, collection: collection1)
+      item2 = Item.create(name: Item.random_name, position: 2, collection: collection1)
+      item3 = Item.create(name: Item.random_name, position: 3, collection: collection1)
+      item4 = Item.create(name: Item.random_name, position: 4, collection: collection1)
+      item5 = Item.create(name: Item.random_name, position: 5, collection: collection1)
 
     
     collection2 = Collection.create(name: 'Collection 2', position: 2)
 
-      item1 = Item.create(name: 'random1', position: 1, collection: collection2)
-      item2 = Item.create(name: 'random2', position: 2, collection: collection2)
-      item3 = Item.create(name: 'random3', position: 3, collection: collection2)
-      item4 = Item.create(name: 'random4', position: 4, collection: collection2)
-      item5 = Item.create(name: 'random5', position: 5, collection: collection2)
+      item1 = Item.create(name: Item.random_name, position: 1, collection: collection2)
+      item2 = Item.create(name: Item.random_name, position: 2, collection: collection2)
+      item3 = Item.create(name: Item.random_name, position: 3, collection: collection2)
+      item4 = Item.create(name: Item.random_name, position: 4, collection: collection2)
+      item5 = Item.create(name: Item.random_name, position: 5, collection: collection2)
 
     collection3 = Collection.create(name: 'Collection 3', position: 3)   
 
-      item1 = Item.create(name: 'random1', position: 1, collection: collection3)
-      item2 = Item.create(name: 'random2', position: 2, collection: collection3)
-      item3 = Item.create(name: 'random3', position: 3, collection: collection3)
-      item4 = Item.create(name: 'random4', position: 4, collection: collection3)
-      item5 = Item.create(name: 'random5', position: 5, collection: collection3)
+      item1 = Item.create(name: Item.random_name, position: 1, collection: collection3)
+      item2 = Item.create(name: Item.random_name, position: 2, collection: collection3)
+      item3 = Item.create(name: Item.random_name, position: 3, collection: collection3)
+      item4 = Item.create(name: Item.random_name, position: 4, collection: collection3)
+      item5 = Item.create(name: Item.random_name, position: 5, collection: collection3)
 
   end
     
- 
+  
+
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ActiveRecord::Base
   belongs_to :collection
+
+def self.random_name
+    name = ""
+    name_length = rand(5..10)
+    
+    name = (1..name_length).map{ ('a'..'z').to_a[rand(26)] }.join
+    
+    return name 
+
+  end
   
 end
